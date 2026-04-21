d3e54dff18b6205f709d50c68040eab03b9c84de
joseph-fantuzzo <joseph.fantuzzo@kuntze.com>
2026-04-21 14:19:19 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'playbook-v1';
+const FILES = [
+  './',
+  './play_calling_app.html',
+  './practice_planner.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
